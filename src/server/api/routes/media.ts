fc031d130117338cab58cf6937e4b79d94a274ec
KakatkarAkshay <akshayakakatkaraa@gmail.com>
2024-05-25 07:43:16 +0000

    feat(api): :wheelchair: Improve the structure of the response for pagination
    * Respond with total number of data as well as maxPages.

--- src/server/api/routes/media.ts
+++ src/server/api/routes/media.ts
@@ -10,7 +10,10 @@ export const mediaRouter = createTRPCRouter({
     .query(async ({ ctx, input: { page } }) => {
       await ctx.db;
       const offset = page ? (page - 1) * 20 : 0;
-      return Media.find().skip(offset).limit(20).lean();
+      const total = await Media.countDocuments();
+      const totalPages = Math.ceil(total / 20);
+      const data = await Media.find().skip(offset).limit(20).lean();
+      return { total, data, totalPages };
     }),
   // Procedure to get a media by its ID
   getMediaById: publicProcedure
@@ -21,11 +24,18 @@ export const mediaRouter = createTRPCRouter({
     }),
   // Search for shows
   searchMedia: publicProcedure
-    .input(z.object({ query: z.string() }))
-    .query(async ({ ctx, input: { query } }) => {
+    .input(z.object({ query: z.string(), page: z.number().default(1) }))
+    .query(async ({ ctx, input: { query, page } }) => {
       await ctx.db;
-      return Media.find({
+      const offset = page ? (page - 1) * 20 : 0;
+      const total = await Media.countDocuments({
         title: { $regex: query, $options: "i" },
-      }).lean();
+      });
+      const totalPages = Math.ceil(total / 20);
+      const data = await Media.find({ title: { $regex: query, $options: "i" } })
+        .skip(offset)
+        .limit(20)
+        .lean();
+      return { total, data, totalPages };
     }),
 });
