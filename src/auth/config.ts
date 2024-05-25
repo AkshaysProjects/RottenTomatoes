d82924d1cd994d62d4f47627532bdea921f010ad
KakatkarAkshay <akshayakakatkaraa@gmail.com>
2024-05-25 07:35:15 +0000

    feat(auth): :sparkles: Use Google and Magic link login
    * RottenTomatoes website uses Google OAuth and Magic Link for login
    * Use Resend for sending the email

--- src/auth/config.ts
+++ src/auth/config.ts
@@ -1,11 +1,11 @@
 import { MongooseAdapter } from "@brendon1555/authjs-mongoose-adapter";
 import { type NextAuthConfig } from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import ResendProvider from "next-auth/providers/resend";
 
 if (!process.env.MONGODB_URI) {
   throw new Error("Please add your Mongo URI to .env.local");
 }
-
 /**
  * Options for Auth.js used to configure adapters, providers, callbacks, etc.
  *
@@ -14,9 +14,13 @@ if (!process.env.MONGODB_URI) {
 export const authConfig: NextAuthConfig = {
   adapter: MongooseAdapter(process.env.MONGODB_URI),
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    ResendProvider({
+      apiKey: process.env.RESEND_API_KEY,
+      from: process.env.RESEND_FROM,
     }),
   ],
 };
