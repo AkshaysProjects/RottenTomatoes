93ce3fab3f4b866a3f5b48e5b8560aaa4600bd3c
KakatkarAkshay <akshayakakatkaraa@gmail.com>
2024-05-25 06:18:13 +0000

    fix(database): :card_file_box: Fixup the model for Media
    * Correct it for the imported data

--- src/models/Media.ts
+++ src/models/Media.ts
@@ -1,9 +1,9 @@
 import { Model, Schema, model, models } from "mongoose";
 
 // Define the media type enum
-enum MediaType {
-  MOVIE = "movie",
-  TV_SHOW = "tv_show",
+export enum MediaType {
+  MOVIE = "Movie",
+  TV_SHOW = "TV Show",
 }
 
 // Define the type for the media schema
@@ -28,26 +28,39 @@ export interface IMedia {
 }
 
 // Define the schema
-const MediaSchema = new Schema({
-  title: { type: String, required: true },
+const MediaSchema = new Schema<IMedia>({
+  title: { type: String, required: true, index: true },
   rating: { type: Number, required: true },
   language: { type: String, required: true },
   runtime: String,
-  releaseDate: Date,
+  releaseDate: { type: Date, validate: [isValidDate, "Invalid release date"] },
   trailerUrl: String,
-  firstAirDate: Date,
-  lastAirDate: Date,
-  status: String,
+  firstAirDate: {
+    type: Date,
+    validate: [isValidDate, "Invalid first air date"],
+  },
+  lastAirDate: { type: Date, validate: [isValidDate, "Invalid last air date"] },
+  status: { type: String, default: "Unknown" },
   genres: { type: [String], required: true },
   summary: { type: String, required: true },
   homepage: { type: String, required: true },
   posterUrl: { type: String, required: true },
   cast: { type: [String], required: true },
   bannerUrl: { type: String, required: true },
-  type: { type: MediaType, required: true },
-  imdbId: { type: String, required: true },
+  type: {
+    type: String,
+    enum: ["movie", "tv_show"],
+    required: true,
+    index: true,
+  },
+  imdbId: { type: String, required: true, unique: true },
 });
 
 // Define the model
 const existingModel: Model<IMedia> = models.Media;
 export default existingModel || model<IMedia>("Media", MediaSchema, "media");
+
+// Custom function to validate dates
+function isValidDate(value: Date): boolean {
+  return value instanceof Date && !isNaN(value.getTime());
+}
