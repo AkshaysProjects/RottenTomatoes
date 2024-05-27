b31e575418fdfbf222527c34a3301d69baa2ef71
KakatkarAkshay <akshayakakatkaraa@gmail.com>
2024-05-27 08:09:19 +0000

    feat(app): :sparkles: Initial grid for media for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,33 @@
 "use client";
 
-import { IMedia } from "@/models";
+import MediaCard from "@/components/MediaCard";
 import { api } from "@/trpc/client";
-import { useState } from "react";
 
 export default function Home() {
-  const [media, setMedia] = useState<IMedia[] | null>(null);
+  // Fetch all media
+  const { data, isLoading, isFetching, isError, error, fetchNextPage } =
+    api.media.getAllMedia.useInfiniteQuery(
+      {},
+      {
+        getNextPageParam: (lastPage) => lastPage.nextCursor,
+      },
+    );
 
-  const mediaQuery = api;
-  return <></>;
+  // If loading, show loading message
+  // TODO: Use Skeleton loader
+  if (isLoading) return <div>Loading...</div>;
+
+  // If error or no data, show error message
+  if (isError || !data) return <div>Error: {error?.message}</div>;
+
+  // Flatten the pages
+  const media = data.pages.map((page) => page.data).flat();
+
+  return (
+    <div className="m-12 grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+      {media.map((item) => (
+        <MediaCard key={item._id.toString()} media={item} />
+      ))}
+    </div>
+  );
 }
---
+++ src/components/MediaCard.tsx
@@ -0,0 +1,72 @@
+import { IMedia } from "@/models";
+import {
+  BookmarkCheckIcon,
+  BookmarkIcon,
+  BookmarkMinusIcon,
+  BookmarkPlusIcon,
+  ClapperboardIcon,
+  Tv2Icon,
+} from "lucide-react";
+import { Types } from "mongoose";
+import Image from "next/image";
+import { useState } from "react";
+
+export interface IMediaCardProps {
+  media: IMedia & { _id: Types.ObjectId };
+}
+
+export default function MediaCard({ media }: IMediaCardProps) {
+  const [isWatchlisted, setIsWatchlisted] = useState(false);
+
+  const toggleWatchlist = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    setIsWatchlisted((prev) => !prev);
+  };
+
+  const MediaIcon = media.type === "Movie" ? ClapperboardIcon : Tv2Icon;
+
+  const releaseDate =
+    media.type === "Movie" ? media.releaseDate : media.firstAirDate;
+  const releaseYear = new Date(releaseDate ?? 0).getFullYear();
+
+  return (
+    <div className="group relative mb-8 mt-4 rounded-lg">
+      <div className="relative h-48 w-full min-w-72 overflow-hidden rounded-xl object-cover">
+        <Image
+          src={media.bannerUrl}
+          alt={media.title}
+          className="rounded-xl object-cover group-hover:opacity-90"
+          fill
+          sizes="100%"
+        />
+      </div>
+      <div className="absolute left-0 w-full p-2">
+        <div>
+          <div className="text-body-m text-light-blue font-light">
+            {releaseYear} • <MediaIcon className="mx-1 inline-block" />{" "}
+            {media.type}
+          </div>
+          <div className="heading-xs">{media.title}</div>
+        </div>
+      </div>
+      <div className="absolute left-0 top-0 w-full p-3">
+        <button
+          className="rounded-md bg-gray-800/80 p-2 hover:bg-gray-700"
+          onClick={toggleWatchlist}
+        >
+          {isWatchlisted ? (
+            <>
+              <BookmarkCheckIcon className="block text-white group-hover:hidden" />
+              <BookmarkMinusIcon className="hidden text-white group-hover:block" />
+            </>
+          ) : (
+            <>
+              <BookmarkIcon className="block text-white group-hover:hidden" />
+              <BookmarkPlusIcon className="hidden text-white group-hover:block" />
+            </>
+          )}
+        </button>
+      </div>
+    </div>
+  );
+}
