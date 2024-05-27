d710f6d0e9eac6c0e6f9ebe6dc1d8d5274a73518
KakatkarAkshay <akshayakakatkaraa@gmail.com>
2024-05-27 19:31:30 +0000

    fix(api): :card_file_box: Fixup the filters schema
    * We need to specify the params are optional even if we provide default value

--- src/schemas/filters.ts
+++ src/schemas/filters.ts
@@ -14,18 +14,19 @@ const defaultFilter = {
 export const filtersSchema = z
   .object({
     // Rating should be an array of two numbers, with default values [0, 100]
-    rating: z.array(z.number()).length(2).default([0, 100]),
+    rating: z.array(z.number()).length(2).default([0, 100]).optional(),
     // Languages should be an array of strings, with default value []
-    languages: z.array(z.string()).default([]),
+    languages: z.array(z.string()).default([]).optional(),
     // ReleaseYear should be an array of two numbers, with default values [1900, current year]
     releaseYear: z
       .array(z.number())
       .length(2)
-      .default([1900, new Date().getFullYear()]),
+      .default([1900, new Date().getFullYear()])
+      .optional(),
     // Status should be an array of strings, with default value []
-    status: z.array(z.string()).default([]),
+    status: z.array(z.string()).default([]).optional(),
     // Genres should be an array of strings, with default value []
-    genres: z.array(z.string()).default([]),
+    genres: z.array(z.string()).default([]).optional(),
     // Type should be an optional enum value of MediaType (MOVIE or TV_SHOW)
     type: z.enum([MediaType.MOVIE, MediaType.TV_SHOW]).optional(),
   })
