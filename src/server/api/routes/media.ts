c1222d15d05fcbf89fd22ba1aa4bbaac9e0f6c50
KakatkarAkshay <akshayakakatkaraa@gmail.com>
2024-05-28 07:16:06 +0000

    feat(api): :sparkles: Allow to fetch available filters

--- src/server/api/routes/media.ts
+++ src/server/api/routes/media.ts
@@ -6,8 +6,9 @@ import { createTRPCRouter, publicProcedure } from "../trpc";
 
 /**
  * TRPC router for media-related procedures:
- * - getAllMedia: Fetches all media items with optional filters and pagination.
+ * - getMedia: Fetches all media items with optional filters and pagination.
  * - getMediaById: Fetches a single media item by its ID.
+ * - getMediaFilters: Fetches available filters for media items.
  */
 export const mediaRouter = createTRPCRouter({
   getMedia: publicProcedure
@@ -33,4 +34,58 @@ export const mediaRouter = createTRPCRouter({
       await ctx.db;
       return Media.findById(id).lean();
     }),
+
+  getMediaFilters: publicProcedure.query(async ({ ctx }) => {
+    await ctx.db;
+
+    // Fetch distinct languages
+    const languages = await Media.distinct("language");
+
+    // Fetch distinct status
+    const status = await Media.distinct("status");
+
+    // Fetch distinct genres
+    const genres = await Media.aggregate([
+      { $unwind: "$genres" },
+      { $group: { _id: "$genres" } },
+    ]).then((res) => res.map((g) => g._id));
+
+    // Fetch min and max release years
+    const releaseYear = await Media.aggregate([
+      {
+        $group: {
+          _id: null,
+          releaseYears: { $addToSet: { $year: "$releaseDate" } },
+          firstAirYears: { $addToSet: { $year: "$firstAirDate" } },
+        },
+      },
+      {
+        $project: {
+          releaseYears: { $setUnion: ["$releaseYears", "$firstAirYears"] },
+        },
+      },
+      { $unwind: "$releaseYears" },
+      {
+        $group: {
+          _id: null,
+          minReleaseYear: { $min: "$releaseYears" },
+          maxReleaseYear: { $max: "$releaseYears" },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          minReleaseYear: 1,
+          maxReleaseYear: 1,
+        },
+      },
+    ]);
+
+    return {
+      languages,
+      status,
+      genres,
+      releaseYear: Object.values(releaseYear[0]) as [number, number],
+    };
+  }),
 });
